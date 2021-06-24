358a457a2a3058b3c6f0e172c33a10817e8ab5cd
myk-recording <myknew@126.com>
2021-06-24 19:56:26 +0800

    add observable

--- Tic Tac Toe(MVC,MVP,MVVM)/JZQ_MVVM/app/src/main/java/com/example/myk/jzq_mvvm/viewmodel/JzqViewModel.java
+++ Tic Tac Toe(MVC,MVP,MVVM)/JZQ_MVVM/app/src/main/java/com/example/myk/jzq_mvvm/viewmodel/JzqViewModel.java
@@ -4,11 +4,30 @@ import android.databinding.ObservableArrayMap;
 import android.databinding.ObservableField;
 
 import com.example.myk.jzq_mvvm.model.Board;
+import com.example.myk.jzq_mvvm.model.Player;
 
 public class JzqViewModel {
 
     private Board board;
     public final ObservableArrayMap<String,String> cells = new ObservableArrayMap<>();
     public final ObservableField<String> winner = new ObservableField<>();
+
+    public JzqViewModel() {
+        board = new Board();
+    }
+
+    public void onResetSelected() {
+        board.restart();
+        winner.set(null);
+        cells.clear();
+    }
+
+    public void onClickedCellAt(int row,int col) {
+        Player player = board.mark(row,col);
+        if (player != null) {
+            cells.put("" + row + col,player.toString());
+            winner.set(board.getWinner() == null ? null : board.getWinner().toString());
+        }
+    }
 
 }
