6f203e827f07ae72640c5a2f835dcc47627ddf7b
myk-recording <myknew@126.com>
2021-07-11 17:48:29 +0800

    create FlowLayout

---
+++ FlowLayout/app/src/main/java/com/example/myk/flowlayout/FlowLayout.java
@@ -0,0 +1,35 @@
+package com.example.myk.flowlayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FlowLayout extends ViewGroup {
+    public FlowLayout(Context context) {
+        super(context);
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View childView = getChildAt(i);
+            LayoutParams layoutParams = childView.getLayoutParams();
+            
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+
+    }
+}
