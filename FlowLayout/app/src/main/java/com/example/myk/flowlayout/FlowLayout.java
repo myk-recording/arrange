9c536cb78255eac506febdcadc53cda99214d580
myk-recording <myknew@126.com>
2021-07-20 19:58:14 +0800

    children mesureSpec

--- FlowLayout/app/src/main/java/com/example/myk/flowlayout/FlowLayout.java
+++ FlowLayout/app/src/main/java/com/example/myk/flowlayout/FlowLayout.java
@@ -1,7 +1,9 @@
 package com.example.myk.flowlayout;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -21,15 +23,25 @@ public class FlowLayout extends ViewGroup {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int childCount = getChildCount();
+        int paddingLeft = getPaddingLeft();
+        int paddingRight = getPaddingRight();
+        int paddingTop = getPaddingTop();
+        int paddingBottom = getPaddingBottom();
         for (int i = 0; i < childCount; i++) {
             View childView = getChildAt(i);
-            LayoutParams layoutParams = childView.getLayoutParams();
-            
+            LayoutParams childLayoutParams = childView.getLayoutParams();
+            int childWidthMeasureSpec = getChildMeasureSpec(widthMeasureSpec,paddingLeft + paddingRight,childLayoutParams.width);
+            int childHeightMeasureSpec = getChildMeasureSpec(heightMeasureSpec,paddingTop + paddingBottom,childLayoutParams.height);
+            childView.measure(childWidthMeasureSpec,childHeightMeasureSpec);
         }
     }
 
     @Override
     protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
 
+    }
+
+    public static int dp2px(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dp, Resources.getSystem().getDisplayMetrics());
     }
 }
